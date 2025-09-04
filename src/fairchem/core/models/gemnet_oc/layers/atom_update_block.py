95b3685cbbf9d31858b7f4605f41d500c7592335
hongmoxian <2428253335@qq.com>
2025-09-04 16:30:48 +0800

    调整AtomUpdateBlock中的MLP层输入特征，增加一个额外特征以优化模型性能，确保电荷和能量计算的一致性。

--- src/fairchem/core/models/gemnet_oc/layers/atom_update_block.py
+++ src/fairchem/core/models/gemnet_oc/layers/atom_update_block.py
@@ -47,7 +47,7 @@ class AtomUpdateBlock(torch.nn.Module):
         self.dense_rbf = Dense(emb_size_rbf, emb_size_edge, activation=None, bias=False)
         self.scale_sum = ScaleFactor()
 
-        self.layers = self.get_mlp(emb_size_edge, emb_size_atom, nHidden, activation)
+        self.layers = self.get_mlp(emb_size_edge, emb_size_atom + 1, nHidden, activation)
 
     def get_mlp(self, units_in: int, units: int, nHidden: int, activation):
         if units_in != units:
